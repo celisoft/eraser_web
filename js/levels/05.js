332af004578f763ed5ddd9389aa13289aedd01e7
Céline Libéral <celine.liberal@mail.com>
2015-05-08 18:04:33 +0200

    Level 04 corrected and level 05 OK

--- js/levels/05.js
+++ js/levels/05.js
@@ -1,7 +1,7 @@
 //Create function required by level 01
-function lvl04_create()
+function lvl05_create()
 {
-	this.game.add.image(0, 0, 'bg_04');
+	this.game.add.image(0, 0, 'default_bg');
 
 	//Activate physic engine
 	this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -10,42 +10,38 @@ function lvl04_create()
 	platforms.enableBody = true;
 
 	//Generating grounds
-	var ground = platforms.create(1*64, 3*64, 'ground')
-	ground.scale.setTo(2, 1);
+	var ground = platforms.create(0, 4*64, 'ground')
+	ground.scale.setTo(6, 1);
 	ground.body.immovable = true;
-  ground.body.checkCollision.down = false;
+ 	ground.body.checkCollision.down = false;
 
-	ground = platforms.create(12*64, 4*64, 'ground');
-	ground.scale.setTo(5, 1);
+	ground = platforms.create(9*64, 4*64, 'ground');
+	ground.scale.setTo(7, 1);
 	ground.body.immovable = true;
-  ground.body.checkCollision.down = false;
+  	ground.body.checkCollision.down = false;
 
-	ground = platforms.create(1*64, 5*64, 'ground');
-	ground.scale.setTo(2, 1);
-	ground.body.immovable = true;
-  ground.body.checkCollision.down = false;
-
-	ground = platforms.create(1*64, 7*64, 'ground');
-	ground.scale.setTo(2, 1);
-	ground.body.immovable = true;
-  ground.body.checkCollision.down = false;
-
-	ground = platforms.create(2*64, 9*64, 'ground');
-	ground.body.immovable = true;
-  ground.body.checkCollision.down = false;
-
-	ground = platforms.create(7*64, 9*64, 'ground');
+	ground = platforms.create(6*64, 6*64, 'ground');
 	ground.scale.setTo(4, 1);
 	ground.body.immovable = true;
-	ground.body.checkCollision.down = false;
+  	ground.body.checkCollision.down = false;
+
+	ground = platforms.create(10*64, 8*64, 'ground');
+	ground.scale.setTo(3, 1);
+	ground.body.immovable = true;
+  	ground.body.checkCollision.down = false;
+
+	ground = platforms.create(1*64, 9*64, 'ground');
+	ground.scale.setTo(8, 1);
+	ground.body.immovable = true;
+  	ground.body.checkCollision.down = false;
 
 	ground = platforms.create(0, 11*64, 'ground');
 	ground.scale.setTo(16, 1);
 	ground.body.immovable = true;
-	ground.body.checkCollision.down = false;
+  	ground.body.checkCollision.down = false;
 
 	//Integrate exit
-	exit_door = this.game.add.sprite(1*64, 1*64, 'hole');
+	exit_door = this.game.add.sprite(14*64, 9*64, 'hole');
 	this.game.physics.arcade.enable(exit_door);
 	exit_door.body.immovable = true;
 
@@ -53,25 +49,38 @@ function lvl04_create()
 	dangers = game.add.group();
 	dangers.enableBody = true;
 
-	var arachne = game.add.sprite(2*64, 7*64+16, 'arachne');
+	var arachne = game.add.sprite(10*64, 8*64+16, 'arachne');
 	arachne.animations.add('moving', [0, 1, 2, 1], 2, true);
 	arachne.animations.play('moving');
 	dangers.add(arachne);
 
-	var plant = game.add.sprite(1*64, 6*64, 'plant');
+	var plant = game.add.sprite(13*64, 3*64, 'plant');
 	plant.animations.add('moving', [1, 0, 1, 2], 1, true);
 	plant.animations.play('moving');
 	dangers.add(plant);
 
+	slimes = game.add.group();
+	slimes.enableBody = true;
+	var slime = game.add.sprite(5*64,10*64, 'monster');
+	slime.animations.add('moving',[0,1,2],1, true);
+	slime.animations.play('moving');
+	slime.scale.x *= -1;
+	slimes.add(slime);
+	slimes_max = 7;
+	slimes_cpt = 0;
+	slimes_dir = 0;
+
 	ghosts = game.add.group();
 	ghosts.enableBody = true;
-	var ghost = ghosts.create(9*64,7*64, 'ghost');
+	var ghost = ghosts.create(9*64,5*64, 'ghost');
 	ghost.body.gravity.y = 250;
+
+	init_pics(6, 9, 5);
 
 	// Timers
 	timers = game.add.group();
 	timers.enableBody = true;
-	var timer = timers.create(2*64,6*64, 'timer');
+	var timer = timers.create(14*64,3*64, 'timer');
 	timer.body.immovable = true;
 
 	//Define the mouse cursor and callback require on move
@@ -80,7 +89,7 @@ function lvl04_create()
 	this.game.input.addMoveCallback(mouse_move, this);
 
 	//Integrate player
-	player = game.add.sprite(12*64, 0, 'playersheet');
+	player = game.add.sprite(64, 3*64, 'playersheet');
 	player.anchor.setTo(0.5, 0.5);
 	this.game.physics.arcade.enable(player);
 	player.body.gravity.y = 450;
@@ -95,9 +104,10 @@ function lvl04_create()
 	level_timer_display = this.game.add.text(this.game.world.width-64,32,level_timer,{fontSize: '32px', fill:'#000000'});
 	this.game.time.events.loop(Phaser.Timer.SECOND, decrease_time, this);
 	this.game.time.events.loop(Phaser.Timer.SECOND*1.25, ghosts_move, this);
+	this.game.time.events.loop(Phaser.Timer.SECOND*0.25, slimes_move, this);
 }
 
-function lvl04_switch()
+function lvl05_switch()
 {
 	platforms.removeAll(true, false);
 	dangers = 0;
@@ -106,18 +116,18 @@ function lvl04_switch()
 	player_dir = 1;
 	key_cursors = 0;
 
-	this.game.state.start('lvl05');
+	this.game.state.start('lvl06');
 }
 
-function lvl04_update()
+function lvl05_update()
 {
 	this.game.physics.arcade.collide(player, platforms);
-
 	this.game.physics.arcade.collide(ghosts, platforms);
 	player_move();
 
-	this.game.physics.arcade.overlap(player, exit_door, lvl04_switch, null, this);
+	this.game.physics.arcade.overlap(player, exit_door, lvl05_switch, null, this);
 	this.game.physics.arcade.overlap(player, timers, timer_collision, null, this);
 	this.game.physics.arcade.overlap(player, dangers, danger_collision, null, this);
 	this.game.physics.arcade.overlap(player, ghosts, danger_collision, null, this)
+	this.game.physics.arcade.overlap(player, slimes, danger_collision, null, this)
 }
